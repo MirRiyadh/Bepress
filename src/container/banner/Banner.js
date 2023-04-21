275b0a84302ab259a4251cf05d2c255e65ec4aa5
Riyadh <riyadh.ju@gmail.com>
2023-04-22 01:36:25 +0600

    added compontents

---
+++ src/container/banner/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Banner = () => {
+  return <div>This is banner part</div>;
+};
+
+export default Banner;
---
+++ src/container/home conatiner/HomeContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeContainer = () => {
+  return <div>this is home content part</div>;
+};
+
+export default HomeContainer;
